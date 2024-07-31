feecc656d9a5eba9d11bd0838455401e4279679f
Hency Dsouza <hencyhd39@gmail.com>
2024-07-31 10:22:38 +0530

    refactor: add routes class

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,18 +1,17 @@
 import express from "express";
 import PostController from "../resources/post/post.controller";
 
-const post = new PostController()
-
 class Routes {
     public router = express.Router()
+    private post = new PostController()
 
     constructor() {
         this.createPostRoutes()
     }
 
     private createPostRoutes() {
-        this.router.post('/posts', post.create)
-        this.router.get('/posts', post.getAllPosts)
+        this.router.post('/posts', this.post.create)
+        this.router.get('/posts', this.post.getAllPosts)
     }
 }
 
